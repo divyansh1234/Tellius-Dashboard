ccc09ea9a7d04929deb871019a31d11c4aaa0068
Divyansh Srivastava <srivastavadivyansh121@gmail.com>
2020-05-07 01:43:25 +0530

    tellius

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Tellius-Dashboard
 A Dashboard consisting of time series data for about 1.5 months from a building
+## Tools used to build this project:
+>React js
+>Highcharts
+>JAVASCRIPT
+![landing page1](/images/landing1.png)
+* This web app loads the data and creates a line chart with date_time,temperature and humidity.
+* Link for the project demo is given below.
+[a link](https://peaceful-perlman-7ceb53.netlify.app/)
+
---
+++ src/components/Charts.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import Data from './data/data.json'
+import ReactHighcharts from 'react-highcharts';
+class Chart extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(props)
+        this.state = {
+            chartData: {
+                chart: {
+                    zoomType: 'xy'
+                },
+                title: {
+                    text: 'charts',
+                    align: 'left'
+                },
+                colors: ['lightblue', 'red',],
+                xAxis: [{
+                    categories: [...Data.map(i => i.date_time)],
+                    crosshair: true
+                }],
+
+                yAxis: [{ // Primary yAxis
+                    labels: {
+                        format: '{value}°C',
+                    },
+                    title: {
+                        text: 'Temperature',
+                    },
+                    opposite: true
+
+                }, { // Secondary yAxis
+                    gridLineWidth: 0,
+                    title: {
+                        text: 'humidity',
+                    },
+                    labels: {
+                        format: '{value} mm',
+                    }
+
+                }],
+                tooltip: {
+                    shared: true
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'left',
+                    x: 80,
+                    verticalAlign: 'top',
+                    y: 55,
+                    floating: true
+                },
+                series: [{
+                    name: 'humidity',
+                    type: 'column',
+                    yAxis: 1,
+                    data: [...Data.map(i => i.env_humidity)],
+                    tooltip: {
+                        valueSuffix: ' mm'
+                    }
+
+                }, {
+                    name: 'Temperature',
+                    type: 'spline',
+                    data: [...Data.map(i => i.env_temp)],
+                    tooltip: {
+                        valueSuffix: ' °C'
+                    }
+                }],
+                responsive: {
+                    rules: [{
+                        condition: {
+                            maxWidth: 500
+                        },
+                        chartOptions: {
+                            legend: {
+                                floating: false,
+                                layout: 'horizontal',
+                                align: 'center',
+                                verticalAlign: 'bottom',
+                                x: 0,
+                                y: 0
+                            },
+                            yAxis: [{
+                                labels: {
+                                    align: 'right',
+                                    x: 0,
+                                    y: -6
+                                },
+                                showLastLabel: false
+                            }, {
+                                labels: {
+                                    align: 'left',
+                                    x: 0,
+                                    y: -6
+                                },
+                                showLastLabel: false
+                            }, {
+                                visible: false
+                            }]
+                        }
+                    }]
+                }
+            }
+
+
+        }
+        console.log(Object.keys(this.state["chartData"]))
+    }
+
+    render() {
+        const config = this.state.chartData;
+        console.log(config)
+        return (
+            <div className="container">
+                <ReactHighcharts config={config} />
+            </div>
+
+        )
+    }
+
+}
+export default Chart
