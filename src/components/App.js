bcad124744a74740c5e069feca680acc3566e6c1
Divyansh Srivastava <srivastavadivyansh121@gmail.com>
2020-05-07 12:34:23 +0530

    up

--- src/components/App.js
+++ src/components/App.js
@@ -2,37 +2,38 @@ import React, { Component } from 'react'
 import Chart from './Charts'
 
 export default class App extends Component {
-    constructor() {
-        super();
-        //local state for data
-        this.state = {
-            chartData: {}
-        }
-    }
+    // constructor() {
+    //     super();
+    //     //local state for data  
+    //     this.state = {
+    //         chartData: {}
+    //     }
+    // }
     //lifecycle hook
-    componentWillMount() {
-        this.getChartData();
-    }
-    getChartData = () => {
-        this.setState({
-            config: {
-                title: {
-                    text: 'Highcharts'
-                },
-                xAxis: {
-                    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-                },
-                series: [{
-                    data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
-                }]
-            }
-        })
-    }
+    // componentWillMount() {
+    //     this.getChartData();
+    // }
+    // getChartData = () => {
+    //     this.setState({
+    //         config: {
+    //             title: {
+    //                 text: 'Highcharts'
+    //             },
+    //             xAxis: {
+    //                 categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    //             },
+    //             series: [{
+    //                 data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    //             }]
+    //         }
+    //     })
+    // }
     render() {
         return (
             <div>
-                <Chart chartData={this.state.chartData} />
+                <Chart  />
             </div>
         )
     }
 }
+// chartData={this.state.chartData}
--- src/components/Charts.js
+++ src/components/Charts.js
@@ -5,106 +5,124 @@ class Chart extends React.Component {
     constructor(props) {
         super(props);
         console.log(props)
-        this.state = {
-            chartData: {
-                chart: {
-                    zoomType: 'xy'
-                },
-                title: {
-                    text: 'charts',
-                    align: 'left'
-                },
-                colors: ['lightblue', 'red',],
-                xAxis: [{
-                    categories: [...Data.map(i => i.date_time)],
-                    crosshair: true
-                }],
-
-                yAxis: [{ // Primary yAxis
-                    labels: {
-                        format: '{value}°C',
+        this.state =
+            {
+                chartData:
+                {
+                    chart: {
+                        zoomType: 'xy',
+                        type: 'line'
                     },
                     title: {
-                        text: 'Temperature',
+                        text: 'charts',
+                        align: 'left'
                     },
-                    opposite: true
+                    colors: ['lightblue', 'red',],
+                    credits: {
+                        // enabled:false,
+                        text: 'code',
+                        href: 'https://github.com/divyansh1234/Tellius-Dashboard',
+                        // position:{
+                        //     align:'left',
+                        //     x:-100,
 
-                }, { // Secondary yAxis
-                    gridLineWidth: 0,
-                    title: {
-                        text: 'humidity',
+                        // },
+                        // style:{
+                        //     fontSize:"15px",
+                        //     color:"red"
+                        // }
+
                     },
-                    labels: {
-                        format: '{value} mm',
-                    }
+                    xAxis: [{
+                        categories: [...Data.map(i => i.date_time)],
+                        crosshair: true
+                    }],
 
-                }],
-                tooltip: {
-                    shared: true
-                },
-                legend: {
-                    layout: 'vertical',
-                    align: 'left',
-                    x: 80,
-                    verticalAlign: 'top',
-                    y: 55,
-                    floating: true
-                },
-                series: [{
-                    name: 'humidity',
-                    type: 'column',
-                    yAxis: 1,
-                    data: [...Data.map(i => i.env_humidity)],
-                    tooltip: {
-                        valueSuffix: ' mm'
-                    }
-
-                }, {
-                    name: 'Temperature',
-                    type: 'spline',
-                    data: [...Data.map(i => i.env_temp)],
-                    tooltip: {
-                        valueSuffix: ' °C'
-                    }
-                }],
-                responsive: {
-                    rules: [{
-                        condition: {
-                            maxWidth: 500
+                    yAxis: [{ // Primary yAxis
+                        labels: {
+                            format: '{value}°C',
                         },
-                        chartOptions: {
-                            legend: {
-                                floating: false,
-                                layout: 'horizontal',
-                                align: 'center',
-                                verticalAlign: 'bottom',
-                                x: 0,
-                                y: 0
-                            },
-                            yAxis: [{
-                                labels: {
-                                    align: 'right',
-                                    x: 0,
-                                    y: -6
-                                },
-                                showLastLabel: false
-                            }, {
-                                labels: {
-                                    align: 'left',
-                                    x: 0,
-                                    y: -6
-                                },
-                                showLastLabel: false
-                            }, {
-                                visible: false
-                            }]
+                        title: {
+                            text: 'Temperature',
+                        },
+                        opposite: true
+
+                    }, { // Secondary yAxis
+                        gridLineWidth: 0,
+                        title: {
+                            text: 'humidity',
+                        },
+                        labels: {
+                            format: '{value} mm',
                         }
-                    }]
+
+                    }],
+                    tooltip: {
+                        shared: true
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'left',
+                        x: 80,
+                        verticalAlign: 'top',
+                        y: 55,
+                        floating: true
+                    },
+                    series: [{
+                        name: 'humidity',
+                        type: 'column',
+                        yAxis: 1,
+                        data: [...Data.map(i => i.env_humidity)],
+                        tooltip: {
+                            valueSuffix: ' mm'
+                        }
+
+                    }, {
+                        name: 'Temperature',
+                        type: 'spline',
+                        data: [...Data.map(i => i.env_temp)],
+                        tooltip: {
+                            valueSuffix: ' °C'
+                        }
+                    }],
+                    responsive: {
+                        rules: [{
+                            condition: {
+                                maxWidth: 500
+                            },
+                            chartOptions: {
+                                legend: {
+                                    floating: false,
+                                    layout: 'horizontal',
+                                    align: 'center',
+                                    verticalAlign: 'bottom',
+                                    x: 0,
+                                    y: 0
+                                },
+                                yAxis: [{
+                                    labels: {
+                                        align: 'right',
+                                        x: 0,
+                                        y: -6
+                                    },
+                                    showLastLabel: false
+                                }, {
+                                    labels: {
+                                        align: 'left',
+                                        x: 0,
+                                        y: -6
+                                    },
+                                    showLastLabel: false
+                                }, {
+                                    visible: false
+                                }]
+                            }
+                        }]
+                    }
                 }
+
+
             }
-
-
-        }
         console.log(Object.keys(this.state["chartData"]))
     }
 
